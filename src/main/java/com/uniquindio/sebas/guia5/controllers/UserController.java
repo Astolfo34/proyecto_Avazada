b69e34fdab96990ae416432190c64d7df545f9d7
davidcr08 <68881672+davidcr08@users.noreply.github.com>
2025-04-27 22:57:44 -0500

    Crud User

--- src/main/java/com/uniquindio/sebas/guia5/controllers/UserController.java
+++ src/main/java/com/uniquindio/sebas/guia5/controllers/UserController.java
@@ -15,14 +15,40 @@ import java.util.Optional;
 public class UserController {
     private final UserServices userServices;
 
+    //CRUD
+    //CREATE
     @PostMapping
     public ResponseEntity<UserResponse>createUser(@RequestBody UserRegistration request){
         return ResponseEntity.ok(userServices.createUser(request));
     }
 
-    @GetMapping
-    public ResponseEntity<UserResponse>getUser(@PathVariable String id) {
+    //Read
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable String id) {
         Optional<UserResponse> user = userServices.getUser(id);
-        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+
+    //Update
+    // UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable String id, @RequestBody UserRegistration request) {
+        Optional<UserResponse> updatedUser = userServices.updateUser(id, request);
+        return updatedUser.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+        boolean deleted = userServices.deleteUser(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build(); // 204 No Content
+        } else {
+            return ResponseEntity.notFound().build();  // 404 Not Found
+        }
+    }
+
+
 }
