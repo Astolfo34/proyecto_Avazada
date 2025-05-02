83e1314a368bc58f1c8ed3db8d72c25e7a8a4252
davidcr08 <68881672+davidcr08@users.noreply.github.com>
2025-05-02 07:42:59 -0500

    coreccion clases

--- src/main/java/com/uniquindio/sebas/guia5/doamin/Categoria.java
+++ src/main/java/com/uniquindio/sebas/guia5/doamin/Categoria.java
@@ -1,10 +1,8 @@
 package com.uniquindio.sebas.guia5.doamin;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 
 @Document("categorias")
 @Getter
@@ -14,9 +12,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Categoria {
+
     @Id
     @EqualsAndHashCode.Include
     private String id;
+
     private String name;
     private String description;
 }
+
