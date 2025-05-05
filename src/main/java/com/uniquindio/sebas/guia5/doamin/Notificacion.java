a23018a076f861eba49085bf1f31e44a7016c236
Astolfo34 <jsebasdroid31@gmail.com>
2025-05-05 09:49:28 -0500

    resolviendo conflictos

--- src/main/java/com/uniquindio/sebas/guia5/doamin/Notificacion.java
+++ src/main/java/com/uniquindio/sebas/guia5/doamin/Notificacion.java
@@ -1,7 +1,8 @@
 package com.uniquindio.sebas.guia5.doamin;
 
-import jakarta.persistence.Id;
+
 import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.format.annotation.DateTimeFormat;
 
