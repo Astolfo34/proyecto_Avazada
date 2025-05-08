e0a64b7ed0fba9b2b5274af68227edea275ac58c
Astolfo34 <jsebasdroid31@gmail.com>
2025-05-08 05:53:39 -0500

    la implementacion de los servicios de reporte,ubicacion,notificacion

--- build/generated/sources/annotationProcessor/java/main/com/uniquindio/sebas/guia5/mappers/UserMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/uniquindio/sebas/guia5/mappers/UserMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-02T07:44:12-0500",
-    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.12.1.jar, environment: Java 21.0.7 (Amazon.com Inc.)"
+    date = "2025-05-08T01:45:02-0500",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.12.1.jar, environment: Java 21.0.6 (Amazon.com Inc.)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
